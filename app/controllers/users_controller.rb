03275ccb880bd0f55fe609ad72e6592793f9ba30
hiroki-miura <mrbigrabu0221@yahoo.co.jp>
2019-08-18 23:55:26 +0900

    add create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,14 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+  # 新規登録するためのアクション
+  def create
+     User.create(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,:age)
+  end
 end
